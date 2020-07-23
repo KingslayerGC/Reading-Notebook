95e906e5df908103c623ce4c5497d9e7d6ad1433
Kingslayer <60292994+KingslayerGC@users.noreply.github.com>
2020-07-23 16:40:13 +0800

    增加

---
+++ python 数据结构与算法/数据结构操作的时间复杂度.py
@@ -0,0 +1,13 @@
+## 比较列表和字典的包含操作用时
+import timeit
+import random
+for i in range(10000,1000001,20000):
+     t = timeit.Timer("random.randrange(%d) in x"%i,
+                      "from __main__ import random,x")
+     x = list(range(i))
+     lst_time = t.timeit(number=1000)
+     x = {j:None for j in range(i)}
+     d_time = t.timeit(number=1000)
+     print("%d,%10.3f,%10.3f" %(i, lst_time, d_time))
+
+
