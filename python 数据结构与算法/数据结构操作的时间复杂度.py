b737d9dcc7baeb93a607a0f088ca24b5283e499a
Kingslayer <60292994+KingslayerGC@users.noreply.github.com>
2020-07-24 15:01:58 +0800

    新增

--- python 数据结构与算法/数据结构操作的时间复杂度.py
+++ python 数据结构与算法/数据结构操作的时间复杂度.py
@@ -1,6 +1,5 @@
 ## 比较列表和字典的包含操作用时
 import timeit
-import random
 for i in range(10000,1000001,20000):
      t = timeit.Timer("random.randrange(%d) in x"%i,
                       "from __main__ import random,x")
---
+++ python 数据结构与算法/队列.py
@@ -0,0 +1,89 @@
+## 队列类
+class Queue:
+    def __init__(self, *item):
+        self.items = []
+        if item != None:
+            for i in item:
+                self.enqueue(i)
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    def dequeue(self):
+        return self.items.pop()
+    def isempty(self):
+        return self.items == []
+    def size(self):
+        return len(self.items)
+
+## 抽杀律游戏
+survivor = Queue('笨蛋','傻瓜','铁憨憨','垃圾','臭猪')
+def lastsurvivor(survivor, num=7):
+    i = 1; print(survivor.items)
+    while survivor.size() != 1 :
+        if i%num != 0:
+            survivor.enqueue(survivor.dequeue())
+        else:
+            survivor.dequeue()
+        #print(survivor.items)
+        i+=1
+    return survivor.items[0]
+
+print(lastsurvivor(Queue('笨蛋','傻瓜','铁憨憨','垃圾','臭猪')))
+
+## 打印模拟
+# 打印机类
+class Printer:
+    def __init__(self, print_speed):
+        self.speed = print_speed
+        self.cutask = None
+        self.timeremain = 0
+    def tick(self):
+        if self.timeremain != 0:
+            self.timeremain = self.timeremain-1
+        if self.timeremain == 0:
+            self.cutask = None
+    def isempty(self):
+        return self.cutask == None
+    def timecount(self):
+        if self.cutask.quality == 1:
+            self.timeremain = self.cutask.pages / self.speed * 2
+        else:
+            self.timeremain = self.cutask.pages / self.speed
+# 打印任务类
+class Task:
+    def __init__(self, demand_quality):
+        self.quality = demand_quality
+        self.pages = random.randrange(1,21)
+        self.timeremain = 0
+
+def prob(n):
+    import random
+    return random.randrange(n) == 0
+
+def simulation(time, print_speed):
+    waittime = []
+    tasks = Queue()
+    printer = Printer(print_speed)
+    for second in range(time):
+        printer.tick()
+        # 180分之1的概率生成新任务
+        if prob(180):
+            # 2分之1的概率为高品质任务
+            task = Task(prob(2))
+            tasks.enqueue(task)
+        # 如果打印机空了且打印任务非空，开始打印队首任务
+        if printer.isempty() and not tasks.isempty():
+            task = tasks.dequeue()
+            printer.cutask = task
+            printer.timecount()
+            waittime.append(printer.timeremain + task.timeremain)
+        # 增加等待队列中每一任务的等待时间
+        for task in tasks.items:
+            task.timeremain = task.timeremain+1
+    if len(waittime) == 0:
+        print("No Costumers")
+    else:
+        print("Average Waiting Time：", str(sum(waittime)/len(waittime)))
+    return printer, tasks
+
+printer, tasks = simulation(3600, 1/6)
+
