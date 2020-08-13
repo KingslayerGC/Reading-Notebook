d1a4f82e4ae5980de16da17346479512e27b01e8
Kingslayer <60292994+KingslayerGC@users.noreply.github.com>
2020-08-13 22:55:33 +0800

    initialize

--- python 数据结构与算法/图与图算法/图结构.py
+++ python 数据结构与算法/图与图算法/图结构.py
@@ -2,15 +2,16 @@
 class Vertex():
     def __init__(self, key):
         self.id = key
-        self.connection = {}
+        self.connections = {}
     def addneighbor(self, key, weight):
-        self.connection[key] = weight
+        self.connections[key] = weight
     def __str__(self):
-        string = ""
-        for key, weight in self.connections:
-            string += ('to %s with weight %s\n'(key, weight))
+        string = "id:%s\n"%self.id
+        for key, weight in self.connections.items():
+            string += ('to %s with weight %s\n'%(key, weight))
+        return string
     def getconnections(self):
-        self.connection.keys()
+        return self.connections.keys()
     def getweight(self, key):
         return self.connections[key]
 
@@ -18,7 +19,33 @@ class Vertex():
 class Graph():
     def __init__(self):
         self.vlist = {}
-    def add(self, vertex):
-        
+    def __contains__(self, key):
+        return key in self.vlist
+    def __iter__(self):
+        return self.vlist.__iter__()
+    def __str__(self):
+        string = ""
+        for key in self:
+            string += self[key].__str__() + "\n"
+        return string[:-1]
+    def __getitem__(self, key):
+        if key in self:
+            return self.vlist[key]
+        else:
+            raise KeyError("key not found")
+    def add(self, key):
+        self.vlist[key] = Vertex(key)
+    def addedge(self, fromkey, tokey, weight):
+        if fromkey in self and tokey in self:
+            self[fromkey].addneighbor(tokey, weight)
+        else:
+            raise KeyError("key not found")
 
-
+graph = Graph()
+graph.add(1)
+graph.add(2)
+graph.add(3)
+graph.addedge(2, 1, 123)
+print(graph)
+for key in graph:
+    print(key)
